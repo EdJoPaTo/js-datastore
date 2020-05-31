baef216ccf0f4860702d65a4b3c8d7acac5fd56f
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-05-31 18:50:25 +0200

    feat(transform): add transformation function
    they can be used to get from one key value type to another

--- source/index.ts
+++ source/index.ts
@@ -2,3 +2,4 @@ export * from './key-value'
 export * from './raw-object'
 
 export * from './cache'
+export * from './transform'
---
+++ source/transform.ts
@@ -0,0 +1,17 @@
+export function mapToRecord<T>(entries: Readonly<ReadonlyMap<string, T>>): Record<string, T> {
+	const result: Record<string, T> = {}
+	for (const key of entries.keys()) {
+		result[key] = entries.get(key)!
+	}
+
+	return result
+}
+
+export function arrayToRecord<T>(entries: ReadonlyArray<{readonly key: string; readonly value: T}>): Record<string, T> {
+	const result: Record<string, T> = {}
+	for (const {key, value} of entries) {
+		result[key] = value
+	}
+
+	return result
+}
