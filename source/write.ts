ce7120d03d2234a1281ae561849feb8eb671af04
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2023-05-20 04:12:49 +0200

    refactor!: migrate to ES modules
    It requires Node.js 18 and is mostly used by me anyway.

--- source/write.ts
+++ source/write.ts
@@ -1,5 +1,5 @@
-import * as stringify from 'json-stable-stringify';
-import * as writeFileAtomic from 'write-file-atomic';
+import stringify from 'json-stable-stringify';
+import writeFileAtomic from 'write-file-atomic';
 
 export async function writeJsonFile(filename: string, data: unknown) {
 	const content = stringify(data, {space: '\t'}) + '\n';
