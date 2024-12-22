82c16dbf8cdc481c3a9a99385e1dff7b63f04ec4
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2024-12-22 11:22:47 +0100

    fix: correctly error when not parsable to JSON
    This can happen for example with undefined or functions. Can only happen when user defined T is problematic which is a user error.

--- source/write.ts
+++ source/write.ts
@@ -2,6 +2,10 @@ import stringify from 'json-stable-stringify';
 import writeFileAtomic from 'write-file-atomic';
 
 export async function writeJsonFile(filename: string, data: unknown) {
-	const content = stringify(data, {space: '\t'}) + '\n';
-	await writeFileAtomic(filename, content, 'utf8');
+	const content = stringify(data, {space: '\t'});
+	if (!content) {
+		throw new Error('cant stringify data to valid JSON: ' + typeof data);
+	}
+
+	await writeFileAtomic(filename, content + '\n', 'utf8');
 }
